1144ba9716863e303c6cecb103248275d01ec745
Jay Turner <jaynicholasturner@gmail.com>
2017-08-19 15:48:37 +0100

    Added excuses

---
+++ eastereggs.py
@@ -0,0 +1,22 @@
+import random
+import discord
+from discord.ext import commands
+
+
+class EasterEggs:
+	"""Easter Eggs"""
+	
+	def __init__(self, bot):
+		self.bot = bot
+
+	def get_display_name(self, member):
+		return member.nick if member.nick else str(member.name)
+
+	@commands.command(pass_context=True)
+	async def excuse(self, ctx):
+		excuses = ['{} is finding socks.', '{} is only '+str(random.randint(1,120))+' minutes away.', 'A zebra is running down the road and this is holding {} up.', '{}’s cat got stuck in the toilet.', "{} is already there. Can't you see them?", "It's raining.", 'Pizzzaaaaaaa 🍕🍍']
+		await self.bot.send_typing(ctx.message.channel)
+		await self.bot.say(random.choice(excuses).format(self.get_display_name(ctx.message.author)))
+		
+def setup(bot):
+    bot.add_cog(EasterEggs(bot))
