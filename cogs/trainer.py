f4c4ee6ced78279bcda4a40e8329c062cbcd295e
Jay Turner <jaynicholasturner@gmail.com>
2017-10-06 12:17:38 +0100

    Only return users in that discord server

--- cogs/trainer.py
+++ cogs/trainer.py
@@ -274,9 +274,10 @@ class trainerdex:
 		await self.bot.send_typing(ctx.message.channel)
 		updates = r.listTrainers()
 		updates.sort(key=lambda x:x.xp, reverse=True)
+		filter1 = list(filter(lambda x: ctx.message.server.get_member(x.discord), updates))
 		embed=discord.Embed(title="Leaderboard")
-		for i in range(min(entries, 25)):
-			embed.add_field(name='{}. {}'.format(i+1, updates[i].username), value="{:,}".format(updates[i].xp))
+		for i in range(min(entries, 25, len(filter1))):
+			embed.add_field(name='{}. {}'.format(i+1, filter1[i].username), value="{:,}".format(filter1[i].xp))
 		await self.bot.edit_message(Message, new_content=str(datetime.date.today()), embed=embed)
 
 #Mod-commands
