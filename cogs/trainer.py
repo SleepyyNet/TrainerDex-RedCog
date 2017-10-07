d7594504a98fc44cdb5f29155dd5519a03db334c
Jay Turner <jaynicholasturner@gmail.com>
2017-10-07 15:08:20 +0100

    Initial convertion, needs testing

--- cogs/trainer.py
+++ cogs/trainer.py
@@ -10,12 +10,11 @@ from collections import namedtuple
 from discord.ext import commands
 from .utils import checks
 from .utils.dataIO import dataIO
-from TrainerDex import Requests
+import TrainerDex
 
 settings_file = 'data/trainerdex/settings.json'
 json_data = dataIO.load_json(settings_file)
 token = json_data['token']
-r = Requests(token)
 
 Difference = namedtuple('Difference', [
 	'old_date',
@@ -32,24 +31,20 @@ class trainerdex:
 	
 	def __init__(self, bot):
 		self.bot = bot
-		self.teams = r.getTeams()
+		self.client = TrainerDex.Client(token)
+		self.teams = self.client.get_teams
 		
-	async def getTrainerID(self, username=None, discord=None, account=None, prefered=True):
+	async def get_trainer(self, username=None, discord=None, account=None, prefered=True):
 		"""Returns a Trainer object for a given discord, trainer username or account id"""
 		
-		listTrainers = r.listTrainers()
-		for trainer in listTrainers:
-			if username:
-				if trainer.username.lower()==username.lower():
-					return trainer
-			elif discord:
-				if trainer.discord==discord and trainer.prefered is True:
-					return trainer
-			elif account:
-				if trainer.account==account and trainer.prefered is True:
-					return trainer
-			else:
-				return None
+		if username:
+			return self.client.get_user_from_username(username)
+		elif discord:
+			return TrainerDex.DiscordUser(discord).owner().trainer()
+		elif account:
+			return TrainerDex.User(account).trainer()
+		else:
+			return None
 		
 	async def getTeamByName(self, team: str):
 		for item in self.teams:
@@ -57,8 +52,7 @@ class trainerdex:
 				return item
 	
 	async def getDiff(self, trainer, days: int):
-		updates = r.getUpdates(trainer.id)
-		updates.sort(key=lambda x:x.time_updated, reverse=True)
+		updates = trainer.all_updates()
 		latest = updates[0]
 		first = updates[-1]
 		reference = []
@@ -71,7 +65,7 @@ class trainerdex:
 					old_date = None,
 					old_xp = None,
 					new_date = latest.time_updated,
-					new_xp = latest.total_xp,
+					new_xp = latest.xp,
 					change_time = None,
 					change_xp = None
 				)
@@ -83,22 +77,21 @@ class trainerdex:
 		print(reference)
 		diff = Difference(
 				old_date = reference.time_updated,
-				old_xp = reference.total_xp,
+				old_xp = reference.xp,
 				new_date = latest.time_updated,
-				new_xp = latest.total_xp,
+				new_xp = latest.xp,
 				change_time = latest.time_updated-reference.time_updated+datetime.timedelta(hours=3),
-				change_xp = latest.total_xp-reference.total_xp
+				change_xp = latest.xp-reference.xp
 			)
 		
 		return diff
 	
 	async def updateCard(self, trainer):
-		team = self.teams[int(trainer.team)]
 		dailyDiff = await self.getDiff(trainer, 1)
-		level=r.trainerLevels(xp=dailyDiff.new_xp)
-		embed=discord.Embed(title=trainer.username, timestamp=dailyDiff.new_date, colour=int(team.colour.replace("#", ""), 16))
-		embed.add_field(name='Level', value=level)
-		embed.add_field(name='XP', value='{:,}'.format(dailyDiff.new_xp-r.trainerLevels(level=level)))
+		level=trainer.level()
+		embed=discord.Embed(title=trainer.username, timestamp=dailyDiff.new_date, colour=int(trainer.team.colour.replace("#", ""), 16))
+		embed.add_field(name='Level', value=level.level)
+		embed.add_field(name='XP', value='{:,}'.format(dailyDiff.new_xp-level.xp_required))
 		if dailyDiff.change_xp and dailyDiff.change_time:
 			gain = '{:,} over {} day'.format(dailyDiff.change_xp, dailyDiff.change_time.days)
 			if dailyDiff.change_time.days!=1:
@@ -124,61 +117,47 @@ class trainerdex:
 		return embed
 		
 	async def profileCard(self, name: str, force=False):
-		trainer = await self.getTrainerID(username=name)
-		if trainer.account is not None:
-			account = r.getUser(trainer.account)
-		else:
-			account = None
-		discordUser = trainer.discord
-		trainer = r.getTrainer(trainer.id)
-		team = self.teams[int(trainer.team)]
-		level=r.trainerLevels(xp=trainer.xp)
+		trainer = await self.get_trainer(username=name)
+		account = trainer.account
+		level=trainer.level()
 		if trainer.statistics is False and force is False:
 			await self.bot.say("{} has chosen to opt out of statistics and the trainer profile system.".format(t_pogo))
 		else:
-			embed=discord.Embed(title=trainer.username, timestamp=trainer.xp_time, colour=int(team.colour.replace("#", ""), 16))
+			embed=discord.Embed(title=trainer.username, timestamp=trainer.update.xp_time, colour=int(trainer.team.colour.replace("#", ""), 16))
 			if account and (account.first_name or account.last_name):
 				embed.add_field(name='Name', value=account.first_name+' '+account.last_name)
-			embed.add_field(name='Team', value=team.name)
-			embed.add_field(name='Level', value=level)
-			embed.add_field(name='XP', value='{:,}'.format(trainer.xp-r.trainerLevels(level=level)))
-			#embed.set_thumbnail(url=team.image)
+			embed.add_field(name='Team', value=trainer.team.name)
+			embed.add_field(name='Level', value=level.level)
+			embed.add_field(name='XP', value='{:,}'.format(trainer.update.xp-level.total_xp))
+			#embed.set_thumbnail(url=trainer.team.image)
 			if trainer.cheater is True:
 				embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/341635533497434112/344984256633634818/C_SesKvyabCcQCNjEc1FJFe1EGpEuascVpHe_0e_DulewqS5nYtePystL4un5wgVFhIw300.png')
 				embed.add_field(name='Comments', value='{} is a known spoofer'.format(trainer.username))
-			embed.set_footer(text="Total XP: {:,}".format(trainer.xp))
+			embed.set_footer(text="Total XP: {:,}".format(trainer.update.xp))
 			await self.bot.say(embed=embed)
 	
-	async def _addProfile(self, mention, username: str, xp: int, team: int, has_cheated=False, currently_cheats=False, name: str=None, prefered=True):
+	async def _addProfile(self, mention, username: str, xp: int, team, has_cheated=False, currently_cheats=False, name: str=None, prefered=True):
 		#Check existance
-		listTrainers = r.listTrainers()
-		for trainer in listTrainers:
-			if trainer.username.lower()==username.lower():
-				await self.bot.say("A record already exists in the database for this trainer")
-				await self.profileCard(name=trainer.username, force=True)
-				return
-		#Create or get auth.User
-		#Create or update discord user
-		listDiscordUsers = r.listDiscordUsers()
+		if self.get_trainer(username=username):
+			await self.bot.say("A record already exists in the database for this trainer. Aborted.")
+			return
+		#Create or get auth.User and discord user
 		discordUser=None
 		if mention.avatar_url=='' or mention.avatar_url is None:
 			avatarUrl = mention.default_avatar_url
 		else:
 			avatarUrl = mention.avatar_url
-		for item in listDiscordUsers:
-			if item.discord_id==mention.id:
-				discordUser=item
-		if discordUser is None:
-			user = r.addUserAccount(username='_'+username, first_name=name)
-			discordUser = r.addDiscordUser(name=mention.name, discriminator=mention.discriminator, id=mention.id, avatar_url=avatarUrl, creation=mention.created_at, user=user)
-		elif discordUser.discord_id==mention.id:
-			user = discordUser.account_id
-			discordUser = r.patchDiscordUser(name=mention.name, discriminator=mention.discriminator, id=mention.id, avatar_url=avatarUrl, creation=mention.created_at)
+		if TrainerDex.DiscordUser(mention.id):
+			discordUser=TrainerDex.DiscordUser(mention.id)
+			user = discordUser.owner
+		elif discordUser==None:
+			user = self.client.create_user(username='_'+username, first_name=name)
+			discordUser = self.client.import_discord_user(name=mention.name, discriminator=mention.discriminator, id=mention.id, avatar_url=avatarUrl, creation=mention.created_at, user=user.id)
 		#create or update trainer
-		trainer = r.addTrainer(username=username, team=team, has_cheated=has_cheated, currently_cheats=currently_cheats, prefered=prefered, account=user)
+		trainer = self.client.create_trainer(username=username, team=team.id, has_cheated=has_cheated, currently_cheats=currently_cheats, prefered=prefered, account=user.id)
 		#create update object
-		update = r.addUpdate(trainer, xp)
-		return user, discordUser, trainer, update
+		update = self.client.create_update(trainer.id, xp)
+		return trainer
 
 
 #Public Commands
@@ -208,17 +187,16 @@ class trainerdex:
 		"""
 		
 		await self.bot.send_typing(ctx.message.channel)
-		trainer = await self.getTrainerID(discord=ctx.message.author.id)
+		trainer = await self.get_trainer(discord=ctx.message.author.id)
 		if trainer is not None:
-			trainer = r.getTrainer(trainer.id)
-			if int(trainer.xp) >= int(xp):
-				await self.bot.say("Error: You last set your XP to {xp:,}, please try a higher number. `ValidationError: {usr}, {xp}`".format(usr= trainer.username, xp=trainer.xp))
+			if int(trainer.update.xp) >= int(xp):
+				await self.bot.say("Error: You last set your XP to {xp:,}, please try a higher number. `ValidationError: {usr}, {xp}`".format(usr= trainer.username, xp=trainer.update.xp))
 				return
 			if trainer.goal_total:
 				if trainer.goal_total<=xp and trainer.goal_total != 0:
 					await self.bot.say(random.choice(levelup).format(goal=trainer.goal_total, member=random.choice(list(ctx.message.server.members)).mention))
-					r.patchTrainer(trainer.id, total_goal=0)
-			update = r.addUpdate(trainer.id, xp)
+					self.client.update_trainer(trainer, total_goal=0)
+			update = self.client.create_update(trainer.id, xp)
 			await asyncio.sleep(1)
 			embed = await self.updateCard(trainer)
 			await self.bot.say(embed=embed)
@@ -236,12 +214,12 @@ class trainerdex:
 		"""
 		
 		await self.bot.send_typing(ctx.message.channel)
-		account = await self.getTrainerID(discord=ctx.message.author.id)
+		account = TrainerDex.DiscordUser(ctx.message.author.id).user
 		if last_name=='..':
 			last_name=' '
 		if account:
-			r.patchUserAccount(account.account, first_name=first_name, last_name=last_name)
-			await self.profileCard(account.username)
+			self.client.update_user(account, first_name=first_name, last_name=last_name)
+			await self.profileCard(account.trainer.username)
 		else:
 			await self.bot.say("Not found!")
 			return
@@ -254,17 +232,16 @@ class trainerdex:
 		"""
 		
 		await self.bot.send_typing(ctx.message.channel)
-		trainer = await self.getTrainerID(discord=ctx.message.author.id)
-		trainer = r.getTrainer(trainer.id)
+		trainer = TrainerDex.DiscordUser(ctx.message.author.id).owner.trainer
 		if which.title()=='Daily':
-			r.patchTrainer(trainer.id, daily_goal=goal)
+			self.client.update_trainer(trainer, daily_goal=goal)
 			await self.bot.say("Daily goal set to {:,}".format(goal))
 		elif which.title()=='Total':
-			if goal>trainer.xp:
-				r.patchTrainer(trainer.id, total_goal=goal)
+			if goal>trainer.update.xp:
+				self.client.update_trainer(trainer, total_goal=goal)
 				await self.bot.say("Total goal set to {:,}".format(goal))
 			else:
-				await self.bot.say("Try something higher than your current XP of {:,}.".format(trainer.xp))
+				await self.bot.say("Try something higher than your current XP of {:,}.".format(trainer.update.xp))
 		else:
 			await self.bot.say("`Please choose 'Daily' or 'Total' for after goal.")
 	
@@ -272,12 +249,16 @@ class trainerdex:
 	async def leaderboard(self, ctx, entries=9):
 		Message = await self.bot.say("Thinking...")
 		await self.bot.send_typing(ctx.message.channel)
-		updates = r.listTrainers()
-		updates.sort(key=lambda x:x.xp, reverse=True)
-		filter1 = list(filter(lambda x: ctx.message.server.get_member(x.discord), updates))
+		trainers = []
+		for user in ctx.message.server.members:
+			try:
+				trainers.append(TrainerDex.DiscordUser(user.id).owner.trainer)
+			except:
+				pass
+		trainers.sort(key=lambda x:x.update.xp, reverse=True)
 		embed=discord.Embed(title="Leaderboard")
-		for i in range(min(entries, 25, len(filter1))):
-			embed.add_field(name='{}. {}'.format(i+1, filter1[i].username), value="{:,}".format(filter1[i].xp))
+		for i in range(min(entries, 25, len(trainers))):
+			embed.add_field(name='{}. {}'.format(i+1, trainers[i].username), value="{:,}".format(trainers[i].xp))
 		await self.bot.edit_message(Message, new_content=str(datetime.date.today()), embed=embed)
 
 #Mod-commands
@@ -303,12 +284,15 @@ class trainerdex:
 		
 		await self.bot.send_typing(ctx.message.channel)
 		mbr = ctx.message.mentions[0]
-		xp = r.trainerLevels(level=level) + xp
+		xp = TrainerDex.Level.from_level(level).total_xp + xp
 		team = await self.getTeamByName(team)
+		if team is None:
+			await self.bot.say("That isn't a valid team. Please ensure that you have used the command correctly.")
+			return
 		if opt.title() == 'Spoofer':
-			await self._addProfile(mbr, name, xp, team.id, has_cheated=True, currently_cheats=True)
+			await self._addProfile(mbr, name, xp, team, has_cheated=True, currently_cheats=True)
 		else:
-			await self._addProfile(mbr, name, xp, team.id)
+			await self._addProfile(mbr, name, xp, team)
 		await self.profileCard(name)
 		
 	@commands.command(pass_context=True, no_pm=True)
@@ -325,12 +309,15 @@ class trainerdex:
 		
 		await self.bot.send_typing(ctx.message.channel)
 		mbr = ctx.message.mentions[0]
-		xp = r.trainerLevels(level=level) + xp
+		xp = TrainerDex.Level.from_level(level).total_xp + xp
 		team = await self.getTeamByName(team)
+		if team is None:
+			await self.bot.say("That isn't a valid team. Please ensure that you have used the command correctly.")
+			return
 		if opt.title() == 'Spoofer':
-			await self._addProfile(mbr, name, xp, team.id, has_cheated=True, currently_cheats=True, prefered=False)
+			await self._addProfile(mbr, name, xp, team, has_cheated=True, currently_cheats=True, prefered=False)
 		else:
-			await self._addProfile(mbr, name, xp, team.id, prefered=False)
+			await self._addProfile(mbr, name, xp, team, prefered=False)
 		await self.profileCard(name)
 		
 	@commands.command(pass_context=True, no_pm=True)
@@ -349,7 +336,7 @@ class trainerdex:
 		"""
 		
 		await self.bot.send_typing(ctx.message.channel)
-		xp = r.trainerLevels(level=level) + xp
+		xp = TrainerDex.Level.from_level(level).total_xp + xp
 		team = await self.getTeamByName(team)
 		if team is None:
 			await self.bot.say("That isn't a valid team. Please ensure that you have used the command correctly.")
@@ -375,9 +362,9 @@ class trainerdex:
 			else:
 				await self.bot.say("{} has been approved, super. They're probably super cool, be nice to them.".format(name))
 				if opt.title() == 'Spoofer':
-					await self._addProfile(mbr, name, xp, team.id, has_cheated=True, currently_cheats=True)
+					await self._addProfile(mbr, name, xp, team, has_cheated=True, currently_cheats=True)
 				else:
-					await self._addProfile(mbr, name, xp, team.id)
+					await self._addProfile(mbr, name, xp, team)
 				await self.profileCard(name)
 
 	@commands.group(pass_context=True)
